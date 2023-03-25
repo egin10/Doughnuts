ef8cdf099ca22d43ad23bb74666c82b7190a5ac2
egin10 <egin066@gmail.com>
2023-03-25 08:43:11 +0700

    Initial Commit

---
+++ Doughnuts/DoughnutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoughnutsApp.swift
+//  Doughnuts
+//
+//  Created by Ginanjar Setia Budi on 25/03/23.
+//
+
+import SwiftUI
+
+@main
+struct DoughnutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
